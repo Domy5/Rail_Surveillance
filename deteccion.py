c67fdb49120928b06a1ef4baf17ccbdf92a5a373
Domy5 <domy.mtz@gmail.com>
2022-10-19 20:27:56 +0200

    Cherry pick Primera Lógica (básica)

--- deteccion.py
+++ deteccion.py
@@ -19,7 +19,7 @@ import datetime
 import torch
 import torchvision
 ##import torch_tensorrt
-
+import imutils
 import time
 # from playsound import playsound
 import numpy as np
@@ -28,13 +28,13 @@ import pandas as pd
 import utiles
 import controler
 
+
 loop_flag = 0
 pos = 0
 numero_fotograma = 0
 fpsmax = 0
 contador = 0
 numero_img = 0
-scale = 0.5
 nombre_ventana = 'TFG Domy 0.39'
 lista_puntos = []
 punto = [0, 0]
@@ -49,6 +49,7 @@ color_amarillo = (0, 255, 255)  # color amarillo en BGR
 color_blanco = (255, 255, 255)  # color blanco en BGR
 color_naranja = (26, 127, 239)  # color naranja en BGR
 color_marron = (37, 73, 141)  # color marron en BGR
+color_gris = (150, 150, 150)  # color marron en BGR
 
 uno = True
 dos = True
@@ -56,7 +57,6 @@ tres = True
 cuatro = True
 cinco = True
 seis = True
-
 
 utiles.borrarPantalla()
 
@@ -66,7 +66,7 @@ parser = argparse.ArgumentParser(description='Detector de objetos, computación 
 
 parser.add_argument('-v', '--version', action="store_true",default=False, help='versión del programa')
 parser.add_argument('-info', '--informacion', action="store_true",default=False, help='información de las versiones de los paquetes usados')
-parser.add_argument('-m', '--mascara', action="store_true",default=False, help='muestra la  mascara')
+parser.add_argument('-m', '--mascara', action="store_true",default=True, help='muestra la  mascara')
 parser.add_argument('-d', '--deteccion', action="store_true",default=False, help='muestra la detecciones de objetos')
 parser.add_argument('-s', '--slicer', action="store_true", default=False,help='muestra barra de desplazamiento (consume muchos recursos)')
 parser.add_argument('-mm', '--mouse', action="store_true", default=False,help='muestra por consola las coordenadas de los click')
@@ -128,9 +128,6 @@ if args.input:
 if args.procesar_imagen == 'gpu':
     gpu_frame = cv2.cuda_GpuMat()  # para usar GPU
 
-if args.mouse:
-    cv2.setMouseCallback(nombre_ventana, utiles.dibujando)
-    
 # Model https://pytorch.org/hub/research-models
 # Paper https://zenodo.org/record/7002879#.YxNFEHbtb30
 
@@ -150,12 +147,12 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5x6')
 # (optional list) filter by class, i.e. = [0, 15, 16] for COCO
 # 0 = persona, 1 = bicicleta, 6 = tren, 36 = skateboard, 26 = handbag, 16 = dog, 24  backpack, 13    bench, 28  suitcase
 model.classes = [0, 6]
-# model.conf = 0.25  # NMS confidence threshold
-# model.iou = 0.45  # NMS IoU threshold (0-1)
+model.conf = 0.3  # NMS confidence threshold   
+model.iou = 0.45  # NMS IoU threshold (0-1)  https://kikaben.com/object-detection-non-maximum-suppression/
 # model.agnostic = False  # NMS class-agnostic
 # model.multi_label = False  # NMS multiple labels per box
 # model.max_det = 1000  # maximum number of detections per image
-# model.amp = False  # Automatic Mixed Precision (AMP) inference
+model.amp = False  # Automatic Mixed Precision (AMP) inference
 
 cv2.namedWindow(nombre_ventana)
 cv2.moveWindow(nombre_ventana, 10, 50)
@@ -177,11 +174,23 @@ print("Tiempo de video      :", video_time)
 
 print("HEIGHT: {}".format(height))
 print("WIDTH: {}".format(width))
+print("")
+print("OSD:")
+print('1:-> Infor Alarma, FPS, N fotos')
+print('2:-> ROI')
+print('3:-> Contornos dentro de ROI por subtracion de fondo')
+print('4:-> Punto pie derecho')
+print('5:-> Contornos en la escena (Personas, trenes, bolsos, carros)')
+print('6:-> Activar mejor rendimiento')
+       
 
 # utiles.borrarPantalla()
 
 if args.slicer:
     cv2.createTrackbar('time', nombre_ventana, 0, frames, utiles.nothing)
+    
+if args.mouse:
+    cv2.setMouseCallback(nombre_ventana, utiles.dibujando)
 
 # fgbg = cv2.bgsegm.createBackgroundSubtractorMOG( )
 # fgbg = cv2.bgsegm.createBackgroundSubtractorMOG(history = 200,nmixtures = 5, backgroundRatio = 0.7,noiseSigma = 0 )
@@ -208,27 +217,29 @@ while True:
     
     if ret == False:
         break
+    
+    contorno = False
+    tren = False
+    persona_en_via = False
 
-    # Mejoramos rendimiento
-    contarfoto += 1
-    if contarfoto % 3 !=0:
-        continue
+    ## ## # Mejoramos rendimiento
+    ## if seis:
+    ##     contarfoto += 1
+    ##     if contarfoto % 2 !=0:
+    ##         print("Mejora rendimoento {}....{}".format(seis, contarfoto))
+    ##         continue
 
     if args.procesar_imagen == 'gpu':
         gpu_frame.upload(frame)  # para usar GPU
         gray = cv2.cuda.cvtColor(gpu_frame, cv2.COLOR_BGR2GRAY)  # para usar GPU
         gray = gray.download()  # para usar GPU
-        #resized = cv2.cuda.resize(gpu_frame, (int(1280 * scale), int(720 * scale)))
     elif args.procesar_imagen == 'cpu':
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     else:
         print("error de procesador de imagenes")
 
-    color = color_verde
-    texto_estado = "Sin Movimiento"
-
-    # detección
-    # detect = model(frame, size = 640)
+    # Detección
+    #detect = model(frame, size = 640)
     detect = model(frame)
 
     # info es un objeto de tipo <class 'pandas.core.frame.DataFrame'>
@@ -240,8 +251,6 @@ while True:
 
     for i in range(info.shape[0]):
         
-        str = info.iat[i, 5]
-
         x0 = info.iat[i, 0]
         y0 = info.iat[i, 1]
         x1 = info.iat[i, 2]
@@ -250,21 +259,26 @@ while True:
         clase = info.iat[i, 5]
         name = info.iat[i, 6]
 
+        # print("Clase: {}, Name: {}, Confiabilidad {}.".format(clase, name, confidence))
+
         if clase == 0:  # Persona
+            
+            # pintamos el un punto en esquina inferior derecha de cada persona detectata
             x = x1.astype(int)
             y = y1.astype(int)
             punto = [x - 10, y - 10]
             if cuatro:
                 # Correción Tutor 28/junio
                 cv2.circle(frame, tuple(punto), 1, color_azul_cyan, 3)
-            if cinco:
-                cv2.imshow(nombre_ventana, np.squeeze(detect.render()))
 
         if clase == 6:  # Tren
-            print("QUE LLEGA EL TREN¡¡¡¡¡")
+            tren = True
+            
+        if cinco:
+            cv2.imshow(nombre_ventana, np.squeeze(detect.render()))
 
     if utiles.punto_en_poligono(punto, area_pts):
-        print("PERSONAS EN LA VIA¡¡¡")
+        persona_en_via = True
         #playsound(AUDIO_ARCHIVO, False)
 
     imAux = np.zeros(shape=(frame.shape[:2]), dtype=np.uint8)
@@ -275,20 +289,18 @@ while True:
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, Kernel)
     fgmask = cv2.dilate(fgmask, None, iterations=2)
 
-    cnts = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)[0]
+    cnts = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+    
     for cnt in cnts:
-        if cv2.contourArea(cnt) > 200:  # 500
+        if cv2.contourArea(cnt) > 250:  # 500
             x, y, w, h = cv2.boundingRect(cnt)
             if tres:
                 # dibuja los contornos que se encuentran
                 cv2.rectangle(frame, (x, y), (x + w, y + h), color_amarillo, 2)
-            texto_estado = 'Alerta Moviemiento'
-            color = color_rojo
-
-    if dos:
-        cv2.drawContours(frame, [area_pts], -1, color,2)  # dibuja area de las vias
-
+                contorno = True
+    
+###############################
+    
     end = time.time()
 
     fps = 1 / (end - start)
@@ -305,12 +317,44 @@ while True:
         fpsmax = fps
         contador = 0
 
+###############################
+
+#  logica .. si hay tren.. si hay personas en la via, si ha caido un objeto
+
+    texto_estado = 'Sin Moviemiento'
+    color = color_verde
+
+    if contorno :
+        texto_estado = 'Precaucion Movimiento'
+        color = color_amarillo
+        
+    if persona_en_via :
+        texto_estado = 'ALERTA Movimiento'
+        color = color_rojo
+        print("PERSONAS EN LA VIA¡¡¡")
+        
+    if  tren and persona_en_via :
+        texto_estado = 'ALERTA Movimiento'
+        color = color_rojo
+        print("LLEGADA DEL TREN¡¡¡¡¡")
+            
+    if  tren :
+        tres = False
+    else:
+        tres = True
+
+###############################
+
     if uno:
-        cv2.putText(frame, texto_estado, (340, 400),cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-        cv2.putText(frame, "FPS: {:.2f}".format(fpsmax), (340, 430),cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
-        cv2.putText(frame, "N fotos: {:.2f}".format(numero_fotograma), (340, 460),cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+      
+       cv2.putText(frame, texto_estado, (height -130, width -240),cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+       cv2.putText(frame, "FPS: {:.2f}".format(fpsmax), (height -130, width -210),cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
+       cv2.putText(frame, "N fotos: {:.2f}".format(numero_fotograma), (height -130, width -180),cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2)
         # cv2.putText(frame, "FPS?: {:.2f}".format(fps1), (10, 120),cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
         # cv2.putText(frame, "Total fotogramas: {:.2f}".format(total_frames), (10, 160),cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+    
+    if dos:
+        cv2.drawContours(frame, [area_pts], -1, color, 2)  # dibuja ROI
         
     if args.mascara:
         cv2.imshow('fgmask', fgmask)
@@ -348,7 +392,9 @@ while True:
     if k == ord('4'):                       # Punto pie derecho  
         cuatro =  False if cuatro else True   
     if k == ord('5'):                       # Contornos en la escena (Personas, trenes, bolsos, carros)
-        cinco = False if cinco else True
+        cinco = False if cinco else True    
+    if k == ord('6'):                       # Activar mejor rendimiento
+        seis = False if seis else True
 
     # Cerrar ventana desde X
     elif cv2.getWindowProperty(nombre_ventana, cv2.WND_PROP_AUTOSIZE) < 1:
