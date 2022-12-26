f8e79efeeb4aabc0a55bdfd559398e055397357c
Domy5 <domy.mtz@gmail.com>
2022-12-26 22:54:22 +0100

    🎈 perf(/scratch): language review

--- scratch/colors_test.py
+++ scratch/colors_test.py
@@ -18,33 +18,28 @@ import time
 import numpy as np
 import pandas as pd
 
-color_verde = (0, 255, 0)  # color verde en BGR
-color_rojo = (0, 0, 255)  # color rojo en BGR
-color_azul = (255, 0, 0)  # color azul en BGR
-color_negro = (0, 0, 0)  # color negro en BGR
-color_azul_cyan = (255, 255, 0)  # color azul_cyan en BGR
-color_amarillo = (0, 255, 255)  # color azul_cyan en BGR
-color_blanco = (255, 255, 255)  # color azul_cyan en BGR
-color_naranja = (26, 127, 239)  # color azul_cyan en BGR
-color_marron = (37, 73, 141)  # color azul_cyan en BGR
+# Colors in BGR
 
-print("empeznado")
+green_color = (0, 255, 0)
+red_color = (0, 0, 255)
+blue_color = (255, 0, 0)
+black_color = (0, 0, 0)
+blue_color_cyan = (255, 255, 0)
+yellow_color = (0, 255, 255)
+white_color = (255, 255, 255)
+orange_color = (26, 127, 239)
+brown_color = (37, 73, 141)
+gray_color = (150, 150, 150)
+
+print("start")
 
 img = np.zeros((900,900,3), np.uint8)
 
 cv2.imshow('image', img)
-cv2.circle(img, (450,450), 300, color_azul, -1)
+cv2.circle(img, (450,450), 300, red_color, -1)
 cv2.imshow('image', img)
-cv2.circle(img, (450,450), 200, color_naranja, -1)
+cv2.circle(img, (450,450), 200, yellow_color, -1)
 
-uno = False
-
-
-uno = uno if False else True
-
-print(uno)
-
-# Mostrar la imagen
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- scratch/draw_line_test.py
+++ scratch/draw_line_test.py
@@ -18,21 +18,21 @@ import time
 import numpy as np
 import pandas as pd
 
-'''En geometría analítica las líneas rectas en un plano pueden ser expresadas mediante una ecuación del tipo y = m x + b, donde x, y son variables en un plano cartesiano. En dicha expresión m  es denominada la "pendiente de la recta" y está relacionada con la  inclinación que toma la recta respecto a un par de ejes que definen el  plano. Mientras que b es el denominado "término independiente" u  "ordenada al origen" y es el valor del punto en el cual la recta corta  al eje vertical en el plano.  
-Cita de https://es.wikipedia.org/wiki/Recta'''
+'''In analytic geometry, straight lines in a plane can be expressed by an equation of the type y = m x + b, where x, y are variables in a Cartesian plane. In said expression m is called the "slope of the line" and is related to the inclination that the line takes with respect to a pair of axes that define the plane. While b is the so-called "independent term" or "ordinate to the origin" and is the value of the point at which the line intersects the vertical axis in the plane.
+Quote from https://es.wikipedia.org/wiki/Recta'''
 
 
-VIDEO_PATH = r'videos_pruebas/'
+VIDEO_PATH = r'video_test/'
 VIDEO = "a1-003 1 minuto 1 via.mkv"
 
-def linea(x0,y0,x1,y1):
+def line(x0,y0,x1,y1):
     x0 =float(x0)
     x1 =float(x1)
     y0 =float(y0)
     y1 =float(y1)
     
     if((x0==x1) and (y0==y1)):
-        print("ES EL MISMO PUNTO")
+        print("IT'S THE SAME POINT")
         return None
     if((x1-x0)!=0):
         m = (y1-y0) / (x1-x0)
@@ -44,18 +44,18 @@ def linea(x0,y0,x1,y1):
 cap = cv2.VideoCapture(f'{VIDEO_PATH}{VIDEO}')
 ret, frame = cap.read()
 
-area_pts = np.array([[0, 195], [350, 0], [384, 0], [252, 480], [0, 480]])
+point_area = np.array([[0, 195], [350, 0], [384, 0], [252, 480], [0, 480]])
 
-# Crea una imagen en negro
-#img = np.zeros(shape=(frame.shape[:2]), dtype=np.uint8)
+# Create a black image
+# img = np.zeros(shape=(frame.shape[:2]), dtype=np.uint8)
 
 img = np.zeros((512,512,3), np.uint8)
 
-diferencia_punto1 = (area_pts[2][0] - area_pts[1][0]) // 2
-diferencia_punto2 = (area_pts[3][0] - area_pts[4][0]) // 2
+difference_point_1 = (point_area[2][0] - point_area[1][0]) // 2
+difference_point_2 = (point_area[3][0] - point_area[4][0]) // 2
 
-p1 = diferencia_punto1 + area_pts[1][0]
-p2 = diferencia_punto2 + area_pts[3][0]
+p1 = difference_point_1 + point_area[1][0]
+p2 = difference_point_2 + point_area[3][0]
 
 # print(punto1)
 # print(punto2)
@@ -72,18 +72,17 @@ p2 = diferencia_punto2 + area_pts[3][0]
 #  (x,y)   
 
 # Pinta poligono
-img = cv2.drawContours(img, [area_pts], -1, (255,0,0), -1)
+img = cv2.drawContours(img, [point_area], -1, (255,0,0), -1)
 # pintar linea
 #img = cv2.line(img, (34, 0), (0, 255), (150), 4)
-img = cv2.line(img, (area_pts[0][0],area_pts[0][1]), (area_pts[3][0],area_pts[3][1]), (0,255 ,0 ), 4)
+img = cv2.line(img, (point_area[0][0],point_area[0][1]), (point_area[3][0],point_area[3][1]), (0,255 ,0 ), 4)
 #img = cv2.line(img, (p1, 0), (0, p2), (0, 0, 255), 1)
-img = cv2.line(img, (p1, 0), (area_pts[0][0]+(area_pts[3][0]-area_pts[0][0])//2, area_pts[3][1]+(area_pts[0][1]-area_pts[3][1])//2), (0, 0, 255), 4)
+img = cv2.line(img, (p1, 0), (point_area[0][0]+(point_area[3][0]-point_area[0][0])//2, point_area[3][1]+(point_area[0][1]-point_area[3][1])//2), (0, 0, 255), 4)
 img = cv2.line(img, (p1, 0), (0, round(513.1908713692945)), (0, 100, 0), 1)
 #img = cv2.line(img, (p1, 0), (0,1500), (0, 0, 255), 4)
 
-linea(p1, 0, area_pts[0][0]+(area_pts[3][0]-area_pts[0][0])//2, area_pts[3][1]+(area_pts[0][1]-area_pts[3][1])//2)
+line(p1, 0, point_area[0][0]+(point_area[3][0]-point_area[0][0])//2, point_area[3][1]+(point_area[0][1]-point_area[3][1])//2)
 
-# Mostrar la imagen
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
